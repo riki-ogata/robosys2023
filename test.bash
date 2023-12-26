ffcee397e25011877f6b5914d4f931527e52a7a6
Riki Ogata <s20c1705pv@s.chibakoudai.jp>
2023-12-27 03:19:08 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -11,7 +11,7 @@ res=0
 
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
-[ "${out}" =  15 \n -15 ] || ng ${LINENO}
+[ "${out}" =  15 ] || ng ${LINENO}
 
 out=$(echo あ | ./plus)
 [ "$?" = 1 ]      || ng ${LINENO}
